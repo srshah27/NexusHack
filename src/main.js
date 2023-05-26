d3dd6990366ddc015571f2815db8ed5a2295fb01
srshah27 <snehilshah27@gmail.com>
2023-05-27 04:15:49 +0530

    domains

--- src/main.js
+++
@@ -1,6 +0,0 @@
-// decrease the opacity of the particles when scrolling down
-$(document).ready(function () {
-  $(window).scroll(function () {
-    $("#bg-img").css("opacity", 1 - $(window).scrollTop() / ($('#bg-img').height() * 0.8));
-  });
-});
