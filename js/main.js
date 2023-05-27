f78418afd3226c1a1bb5356dd9c38c6aaaca8e9b
srshah27 <snehilshah27@gmail.com>
2023-05-27 17:27:46 +0530

    terminal preload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ $(document).ready(function () {
     $("#bg-img").css("opacity", 1 - $(window).scrollTop() / ($('#bg-img').height() * 0.8));
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Hide the preloader once the page content has loaded
+  var preloader = document.querySelector('.preloader');
+  preloader.style.display = 'none';
+
+  // Add a 'loaded' class to the body to trigger the CSS rule
+  document.body.classList.add('loaded');
+});
